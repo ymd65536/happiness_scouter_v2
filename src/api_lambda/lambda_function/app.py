df1fc5377301e9f858666f5ae39f7111a6f48979
ymd65536 <ymd65536@gmail.com>
2022-12-17 06:13:33 +0900

    fix: テーブル名が設定されていない場合の処理を追加

--- src/api_lambda/lambda_function/app.py
+++ src/api_lambda/lambda_function/app.py
@@ -34,7 +34,7 @@ def get_dynamo_table(table_name):
     return dynamo_table
 
 
-def handler(event, context):
+def lambda_handler(event, context):
 
     body = json.loads(event['body'])
 
@@ -48,6 +48,16 @@ def handler(event, context):
     # bodyからevent を取得する
     body_event = body['events'][0]
     reply_token = body_event['replyToken']
+
+    # DynamoDBのテーブル名が設定されていない場合はメッセージを出して終了
+    if table_name == "":
+        response_message = "現在、サービスを停止しております。"
+        line_bot_api.reply_message(
+            reply_token,
+            TextSendMessage(text=response_message))
+
+        return
+
     message_type = body_event['message']['type']
     event_type = body_event['type']
     message_id = body_event['message']['id']
